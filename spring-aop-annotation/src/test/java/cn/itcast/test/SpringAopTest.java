5c0abaaa2867cee018c23c15483d895101d0d22a
JustinHan <>
2021-09-29 16:37:12 +0800

    spring基于注解的aop

---
+++ spring-aop-annotation/src/test/java/cn/itcast/test/SpringAopTest.java
@@ -0,0 +1,15 @@
+package cn.itcast.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringAopTest {
+
+    @Test
+    public void test1() {
+
+    }
+}
